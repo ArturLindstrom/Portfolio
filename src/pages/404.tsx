b6221b8fef1aadd8ce779a3c835918adaf328460
arturlindstrom <artur.lindstrom@gmail.com>
2023-01-09 11:29:15 +0100

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,50 +1,17 @@
+import { Link } from "gatsby";
 import * as React from "react";
-import { Link, HeadFC, PageProps } from "gatsby";
+import Layout from "../components/Layout";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
-
-const NotFoundPage: React.FC<PageProps> = () => {
+export default function NotFoundPage() {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : //
-        null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <div className="flex flex-col items-center justify-center h-[80vh]">
+        <h1 className="text-5xl font-bold text-white">404</h1>
+        <p className="text-2xl font-semibold text-white">Page not found</p>
+        <Link to="/" className="mt-4 text-5xl font-bold text-white underline">
+          Back To Home
+        </Link>
+      </div>
+    </Layout>
   );
-};
-
-export default NotFoundPage;
-
-export const Head: HeadFC = () => <title>Not found</title>;
+}
