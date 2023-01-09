7d245f7f9ecef0646906fde85ffd19dc1a42a4bb
arturlindstrom <artur.lindstrom@gmail.com>
2023-01-09 09:30:57 +0100

    asd

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { graphql, Link } from "gatsby";
+import { graphql } from "gatsby";
 import { renderRichText } from "gatsby-source-contentful/rich-text";
 import { ContactType } from "../types/contactTypes";
 import Layout from "../components/Layout";
