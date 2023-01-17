d98b2b78c7f293368ea2755a16a38a00913659c1
arturlindstrom <artur.lindstrom@gmail.com>
2023-01-17 17:01:31 +0100

    add active link to nav

--- src/components/BurgerMenu.tsx
+++ src/components/BurgerMenu.tsx
@@ -81,7 +81,7 @@ export default function BurgerMenu() {
             key={link.node.name}
             onClick={toggleMenu}
             tabIndex={-1}
-            activeClassName="text-primary"
+            activeClassName="text-primary underline"
           >
             {link.node.name}
           </Link>
@@ -93,7 +93,7 @@ export default function BurgerMenu() {
             to={`/${link.node.slug}`}
             key={link.node.slug}
             tabIndex={-1}
-            activeClassName="text-primary"
+            activeClassName="text-primary underline"
           >
             {link.node.title}
           </Link>
